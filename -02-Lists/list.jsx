094b167f14ef84d9d960f442f6da4924502563aa
CHARLES DAUDA <108020360+charlesdauda@users.noreply.github.com>
2025-09-14 15:52:55 +0000

    Update list.jsx

--- -02-Lists/list.jsx
+++ -02-Lists/list.jsx
@@ -1,1 +1,18 @@
+import React from 'react'
 
+
+const Num = () => {
+    const number = [1,2,3,4,5,6];
+
+    return (
+        <main>
+            {number.map((number) =>(
+                <ul key={number}>
+                    <li>{number}</li>
+                </ul>
+            ))}
+        </main>
+    )
+};
+
+export default Num ;
