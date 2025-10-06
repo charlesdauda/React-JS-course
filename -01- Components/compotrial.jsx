87d1aab0e41723e8302dc7846d5c6d2b96e077fd
CHARLES DAUDA <108020360+charlesdauda@users.noreply.github.com>
2025-10-06 00:34:02 +0000

    Initial Commit

---
+++ -01- Components/compotrial.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import StudentDirctoryApp from './directory(props)';
+
+
+const MyApplication = () => {
+    return (
+        <section>
+            <h1>An overview of components</h1>
+            <StudentDirctoryApp />
+        </section>
+    )
+}
+
+export default MyApplication;
