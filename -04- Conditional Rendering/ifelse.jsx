ff3a08f65ab28b833b5979a94a2a1f5bebf1ae5f
CHARLES DAUDA <108020360+charlesdauda@users.noreply.github.com>
2025-10-07 16:55:18 +0000

    Initial Commit

---
+++ -04- Conditional Rendering/ifelse.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ValidPassword = () => <h1>Valid Password</h1>
+
+const InvalidPassword = () => <h1>Invalid Password</h1>
+
+const Password = ({isValid}) => {
+    if (isValid) {
+        return <ValidPassword/>
+    }
+
+    return <InvalidPassword />
+}
+
+const PasswordApp = () =>{
+    return (
+        <section>
+            <Password isValid = {true}/>
+        </section>
+    )
+}
+
+export default PasswordApp;
