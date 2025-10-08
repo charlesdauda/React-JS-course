fac207266eb8a5c1517429010b8fc26451ead8c7
CHARLES DAUDA <108020360+charlesdauda@users.noreply.github.com>
2025-10-08 14:53:28 +0000

    Initial Commit

---
+++ -05-Inline Styling/styling.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Style = () => {
+
+    return (
+        <section>
+            <h1 style={{color:'black', backgroundColor: 'cyan', padding:'2rem', textAlign: 'center'}}>This is an Inline Styling</h1>
+        </section>
+    )
+}
+
+export default Style;
