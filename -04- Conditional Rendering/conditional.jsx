5a1280ae427a824a82f6852e0338f1ea3357ad9e
CHARLES DAUDA <108020360+charlesdauda@users.noreply.github.com>
2026-02-12 15:20:28 +0000

    Update conditional.jsx

--- -04- Conditional Rendering/conditional.jsx
+++ -04- Conditional Rendering/conditional.jsx
@@ -1,1 +1,24 @@
+import React from 'react';
 
+const ValidPassword = () => <h1>Valid Password</h1>
+const InvalidPassword = () => <h1>Invalid Password</h1>
+
+
+const Password = ({isvalid}) => {
+    if(isvalid){
+        return <ValidPassword />
+    }
+
+    return <InvalidPassword />
+}
+
+const PassApp = () => {
+
+    return (
+        <section>
+            <Password isValid = {true}/>
+        </section>
+    )
+};
+
+export default PassApp;
